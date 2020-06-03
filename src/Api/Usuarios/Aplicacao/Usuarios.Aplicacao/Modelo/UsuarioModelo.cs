4781df784d3e17f719e2e43ef9610135518407a2
Felipe Michel de Azevedo <felipe.azevedoit@outlook.com>
2020-06-03 12:17:08 -0300

    Adicionando modelo usuario (viewModel Aplicacao)

---
+++ src/Api/Usuarios/Aplicacao/Usuarios.Aplicacao/Modelo/UsuarioModelo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Usuarios.Aplicacao.Modelo
+{
+    public class UsuarioModelo
+    {
+        [Key]
+        public int Id { get;  set; }
+
+        [Required(ErrorMessage = "Por favor preencha o campo Nome")]
+        [MinLength(2)]
+        [MaxLength(100)]
+        public string Nome { get;  set; }
+
+        [MinLength(2)]
+        [MaxLength(100)]
+        public string Sobrenome { get; set; }
+
+        [EmailAddress]
+        [DisplayName("E-mail")]
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Por favor preencha a data de Nascimento")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        [DataType(DataType.Date, ErrorMessage = "Data em formato inválido")]
+        [DisplayName("Data Nascimento")]
+        public DateTime DataNascimento { get;  set; }
+
+        public int Escolaridade { get;  set; }
+    }
+}
