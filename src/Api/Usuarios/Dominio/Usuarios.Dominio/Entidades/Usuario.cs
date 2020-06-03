5352caf52b20a9f77db13b7b5f7def8dddb26c90
Felipe Michel de Azevedo <felipe.azevedoit@outlook.com>
2020-06-03 10:18:20 -0300

    Criando estruturas

---
+++ src/Api/Usuarios/Dominio/Usuarios.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Usuarios.Dominio.Entidades
+{
+    public class Usuario
+    {
+        #region Propriedades
+        public int Id { get; protected set ; }
+        public string Nome { get; protected set; }
+        public string Sobrenome { get; protected set; }
+        public DateTime DataNascimento { get; protected set; }
+        public int Escolaridade { get; protected set; }
+        #endregion
+
+        #region Construtores
+
+        public Usuario(int id, string nome, string sobrenome, DateTime dataNascimento, int escolaridade)
+        {
+            Id = id;
+            Nome = nome;
+            Sobrenome = sobrenome;
+            DataNascimento = DataNascimento;
+            Escolaridade = escolaridade;
+        }
+
+        public Usuario()
+        {
+
+        }
+
+        #endregion
+    }
+}
