8d953d6896ff5387dd461581975457293185caad
Felipe Michel de Azevedo <felipe.azevedoit@outlook.com>
2020-06-03 10:38:45 -0300

    Configurando Contexto

---
+++ src/Api/Usuarios/Infraestrutura/Dados/Usuarios.Infraestrutura.Dados/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Usuarios.Dominio.Entidades;
+
+namespace Usuarios.Infraestrutura.Dados
+{
+    public class Contexto: DbContext
+    {
+        public DbSet<Usuario> Usuario { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options)
+        : base(options)
+        {
+
+        }
+    }
+}
