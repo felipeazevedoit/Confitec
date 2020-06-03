22db21b10d27e07bf0c644ebf93b9399dc438918
Felipe Michel de Azevedo <felipe.azevedoit@outlook.com>
2020-06-03 12:26:17 -0300

    Adicionando mapeamento

---
+++ src/Api/Usuarios/Aplicacao/Usuarios.Aplicacao/Mapeamento/DominioParaModelo.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Usuarios.Aplicacao.Modelo;
+using Usuarios.Dominio.Entidades;
+
+namespace Usuarios.Aplicacao.Mapeamento
+{
+    public class DominioParaModelo: Profile
+    {
+        public DominioParaModelo()
+        {
+            CreateMap<Usuario, UsuarioModelo>();
+        }
+    }
+}
---
+++ src/Api/Usuarios/Aplicacao/Usuarios.Aplicacao/Mapeamento/ModeloParaDominio.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Usuarios.Aplicacao.Modelo;
+using Usuarios.Dominio.Entidades;
+
+namespace Usuarios.Aplicacao.Mapeamento
+{
+   public class ModeloParaDominio: Profile
+    {
+        public ModeloParaDominio()
+        {
+            CreateMap<UsuarioModelo, Usuario>()
+                .ConstructUsing(c => new Usuario(c.Id, c.Nome, c.Sobrenome, c.DataNascimento, c.Escolaridade));
+        }
+    }
+}
