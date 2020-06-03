7388e0cea94276bd1e6c2fb4e998d811ddf5a1fc
Felipe Michel de Azevedo <felipe.azevedoit@outlook.com>
2020-06-03 15:36:00 -0300

    Adicionando base de dados a solucao

---
+++ src/Api/Usuarios/Usuarios.Banco/dbo/Tables/Usuario.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Usuario] (
+    [Id]             INT           IDENTITY (1, 1) NOT NULL,
+    [Nome]           VARCHAR (100) NOT NULL,
+    [Sobrenome]      VARCHAR (200) NULL,
+    [Email]          VARCHAR (300) NOT NULL,
+    [DataNascimento] DATE          NULL,
+    [Escolaridade]   INT           NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
